b032ec6cd9eaa7fe1dd516f7789f2e28866974c8
Nishath <nishathmohamed786@gmail.com>
2024-08-07 23:28:47 +0530

    Added spotlight in home component

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -21,12 +21,6 @@ const Navbar = async (props: Props) => {
       <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
         <ul className="flex items-center gap-4 list-none">
           <li>
-            <Link href="#">Products</Link>
-          </li>
-          <li>
-            <Link href="#">Pricing</Link>
-          </li>
-          <li>
             <Link href="#">Clients</Link>
           </li>
           <li>
@@ -34,9 +28,6 @@ const Navbar = async (props: Props) => {
           </li>
           <li>
             <Link href="#">Documentation</Link>
-          </li>
-          <li>
-            <Link href="#">Enterprise</Link>
           </li>
         </ul>
       </nav>
